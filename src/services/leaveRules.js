a4a6afec91d6d72af85e452e60efbaf269a260e0
Desmond Kwaramba <desmondkwaramba1@gmail.com>
2026-06-30 11:48:04 +0200

    add getWorkingDaysInRange function stripping weekends

--- src/services/leaveRules.js
+++ src/services/leaveRules.js
@@ -9,5 +9,24 @@ function isWorkingDay(dateString) {
 
     return !isWeekend && !isHoliday;
 }
+function getWorkingDaysInRange(startDate, endDate) {
+    const days = [];
+    let current = new Date(startDate);
+    const end = new Date(endDate);
 
-module.exports = { isWorkingDay };
+    while (current <= end) {
+        const dateString = current.toISOString().split('T')[0];
+
+        const dayOfWeek = current.getUTCDay();
+        const isWeekend = dayOfWeek === 0 || dayOfWeek === 6;
+
+        if (!isWeekend) {
+            days.push(dateString);
+        }
+
+        current.setUTCDate(current.getUTCDate() + 1);
+    }
+
+    return days;
+}
+module.exports = { isWorkingDay, getWorkingDaysInRange };
